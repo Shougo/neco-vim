31e5725ab5666396921d86bcec2f0b6ea0028ce7
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-08-28 17:49:15 +0900

    Use autoload

---
+++ denops/ddc-sources/necovim.ts
@@ -0,0 +1,28 @@
+import {
+  BaseSource,
+  Candidate,
+  Context,
+} from "https://deno.land/x/ddc_vim@v0.3.0/types.ts";
+import { Denops } from "https://deno.land/x/ddc_vim@v0.3.0/deps.ts";
+
+export class Source extends BaseSource {
+  isBytePos = true;
+
+  async getCompletePosition(args: {
+    denops: Denops,
+    context: Context,
+  }): Promise<number> {
+    return await args.denops.call(
+      'necovim#get_complete_position', args.context.input) as number;
+  }
+
+  async gatherCandidates(args: {
+    denops: Denops,
+    context: Context,
+    completeStr: string,
+  }): Promise<Candidate[]> {
+    return await args.denops.call(
+        'necovim#gather_candidates',
+        args.context.input, args.completeStr) as Candidate[];
+  }
+}
